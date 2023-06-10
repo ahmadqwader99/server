8ad161afca73c4a7ddfca49380d488330bbf51c6
ahmadqwader99 <ahmadqwader2017@gmail.com>
2023-06-10 14:58:06 +0300

    commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./App");
-
-const port = 7700;
+// const port = 7700;
+const port = process.env.port || 7700;
 const server = http.createServer(app);
 module.exports = server;
 app.listen(port);
